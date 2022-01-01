e9ce6a94a70555e1ee0d53c8b1c0a507388512c7
git init <butkepavan55@gmail.com>
2022-01-01 21:28:20 +0530

    alerts done

---
+++ src/context/types.js
@@ -0,0 +1,9 @@
+
+export const SEARCH_SHOWS = "SEARCH_SHOWS";
+export const SET_LOADING = "SET_LOADING";
+export const SET_TV_SHOW = "SET_TV_SHOW";
+export const CLEAR_TV_SHOW = "CLEAR_TV_SHOW";
+
+
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
